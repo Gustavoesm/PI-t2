5a1df1ee97ff4e54b1211770a63744541f655d72
Gustavo Eugênio <gustavo.eugenio@hotmail.com>
2020-12-13 23:50:43 -0300

    ex1 updated and running

--- ex1.m
+++ ex1.m
@@ -5,7 +5,6 @@ image = imread('rice_binary.png');
 [lin, col]=size(image);
 janela=3;
 
-
 north = [1 1 1 ; 1 -2 1; -1 -1 -1]; %NORTE
 south = [-1 -1 -1 ; 1 -2 1; 1 1 1]; %SUL
 east = [-1 1 1 ; -1 -2 1; -1 1 1]; %LESTE
@@ -15,12 +14,13 @@ northwest = [1 1 1 ; 1 -2 -1; 1 -1 -1]; %NOROESTE
 southeast = [-1 -1 1 ; -1 -2 1; 1 1 1]; %SUDESTE
 southwest = [1 -1 -1 ; 1 -2 -1; 1 1 1]; %SUDOESTE
 
-
-
 for i=1:lin
     for j=1:col
         north_sum_cir = south_sum_cir = east_sum_cir = west_sum_cir = 0;
         northeast_sum_cir = northwest_sum_cir = southeast_sum_cir = southwest_sum_cir = 0;
+        north_sum_lin = south_sum_lin = east_sum_lin = west_sum_lin = 0;
+        northeast_sum_lin = northwest_sum_lin = southeast_sum_lin = southwest_sum_lin = 0;
+
         u = 1;
         for linha=i-1:i+1
             p = 1;
@@ -32,27 +32,26 @@ for i=1:lin
                         indice_linha = lin-1;
                     elseif (linha > lin)
                         indice_linha = 1;
-                    else
-                        indice_linha = linha;
                     endif
 
                     if (coluna < 1)
                         indice_coluna = col-1;
                     elseif (coluna > col)
                         indice_coluna = 1;
-                    else
-                        indice_coluna = coluna;
                     endif
                 else
-                    north_sum_lin = north_sum_lin + (image(indice_linha,indice_coluna) * north(u,p));
-                    south_sum_lin = south_sum_lin + (image(indice_linha,indice_coluna) * south(u,p));
-                    east_sum_lin = east_sum_lin + (image(indice_linha,indice_coluna) * east(u,p));
-                    west_sum_lin = west_sum_lin + (image(indice_linha,indice_coluna) * west(u,p));
+                    indice_linha = linha;
+                    indice_coluna = coluna;
 
-                    northeast_sum_lin = northeast_sum_lin + (image(indice_linha,indice_coluna) * northeast(u,p));
-                    northwest_sum_lin = northwest_sum_lin + (image(indice_linha,indice_coluna) * northwest(u,p));
-                    southeast_sum_lin = southeast_sum_lin + (image(indice_linha,indice_coluna) * southeast(u,p));
-                    southwest_sum_lin = southwest_sum_lin + (image(indice_linha,indice_coluna) * southwest(u,p));
+                    north_sum_lin = north_sum_lin + (image(linha,coluna) * north(u,p));
+                    south_sum_lin = south_sum_lin + (image(linha,coluna) * south(u,p));
+                    east_sum_lin = east_sum_lin + (image(linha,coluna) * east(u,p));
+                    west_sum_lin = west_sum_lin + (image(linha,coluna) * west(u,p));
+
+                    northeast_sum_lin = northeast_sum_lin + (image(linha,coluna) * northeast(u,p));
+                    northwest_sum_lin = northwest_sum_lin + (image(linha,coluna) * northwest(u,p));
+                    southeast_sum_lin = southeast_sum_lin + (image(linha,coluna) * southeast(u,p));
+                    southwest_sum_lin = southwest_sum_lin + (image(linha,coluna) * southwest(u,p));
                 end
 
                 north_sum_cir = north_sum_cir + (image(indice_linha,indice_coluna) * north(u,p));
@@ -68,6 +67,16 @@ for i=1:lin
             end
             u = u + 1;
         end
+        image_north_lin(i,j)= north_sum_lin;
+        image_south_lin(i,j)= south_sum_lin;
+        image_east_lin(i,j)= east_sum_lin;
+        image_west_lin(i,j)= west_sum_lin;
+
+        image_northeast_lin(i,j)= northeast_sum_lin;
+        image_northwest_lin(i,j)= northwest_sum_lin;
+        image_southeast_lin(i,j)= southeast_sum_lin;
+        image_southwest_lin(i,j)= southwest_sum_lin;
+
         image_north_cir(i,j)= north_sum_cir;
         image_south_cir(i,j)= south_sum_cir;
         image_east_cir(i,j)= east_sum_cir;
@@ -89,3 +98,13 @@ imwrite(image_northeast_cir,'output/northeast_circular.png')
 imwrite(image_northwest_cir,'output/northwest_circular.png')
 imwrite(image_southeast_cir,'output/southeast_circular.png')
 imwrite(image_southwest_cir,'output/southwest_circular.png')
+
+imwrite(image_north_lin,'output/north_linear.png')
+imwrite(image_south_lin,'output/south_linear.png')
+imwrite(image_east_lin,'output/east_linear.png')
+imwrite(image_west_lin,'output/west_linear.png')
+
+imwrite(image_northeast_lin,'output/northeast_linear.png')
+imwrite(image_northwest_lin,'output/northwest_linear.png')
+imwrite(image_southeast_lin,'output/southeast_linear.png')
+imwrite(image_southwest_lin,'output/southwest_linear.png')
